848f4d2cf2a6b7376398f1e2cdb89f599b0c6cd0
snhkn <senihaakin@gmail.com>
2026-08-25 15:56:25 +0300

    Add profile links to post authors

--- src/components/shared/Post.jsx
+++ src/components/shared/Post.jsx
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom";
+
 const Post = ({ post }) => {
   return (
     <div className="bg-[rgba(255,255,255,0.05)] p-4 rounded-lg shadow">
-      <div className="font-semibold text-lg text-blue-400">{post.author} says:</div>
+      <div className="font-semibold text-lg text-blue-400">
+        <Link to={`/profile/${post.userId}`}>{post.author}</Link>{" "}
+        says:</div>
       <p className="mt-2 text-gray-300">{post.body}</p>
       <div className="text-gray-400 text-sm mt-1">
         {new Date(post.createdAt).toLocaleString()}
